9a9b93a0e22f0b5b3e0969291a35d340d1c69fb1
root <root@Faithtower-Media.localdomain>
2019-10-24 12:32:27 +0000

    added functions

--- src/io/turtabl/Main.java
+++ src/io/turtabl/Main.java
@@ -1,11 +1,27 @@
 package io.turtabl;
 
 public class Main {
-
-    //todo: work on the main method
     public static void main(String[] args) {
+
          System.out.println("Hello, turntabl!");
+
+         if(args.length == 0){
+             System.out.println("Please add some arguments");
+         }else{
+             printArgs(args);
+         }
     }
+
+    public static void printArgs(String[] args){
+        for (String arg : args){
+            printArg(arg);
+        }
+    }
+
+    public static void printArg(String arg){
+        System.out.println(arg);
+    }
+
 }
 
 
